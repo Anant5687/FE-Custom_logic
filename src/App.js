fce946b3cc38b035b516b5a0eca1506cc7c6050e
Anant Jindal <anantjindal45@gmail.com>
2026-06-08 22:11:13 +0530

    Created nested reply handeler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import AutoComplete from "./components/AutoComplete";
-import InfiniteScroll from "./components/InfiniteScroll";
+import NestedComments from "./components/NestedComments";
+// import AutoComplete from "./components/AutoComplete";
+// import InfiniteScroll from "./components/InfiniteScroll";
 
 function App() {
   return (
     <div style={{ padding: "4px" }}>
-      <AutoComplete />
-      <InfiniteScroll />
+      {/* <AutoComplete />
+      <InfiniteScroll /> */}
+      <NestedComments />
     </div>
   );
 }
